97f03da408f432a2ab746152976170b86c29e9b8
llrebecca21 <llrebecca21@gmail.com>
2024-04-04 21:21:48 -0500

    update details on variables, delete export

--- R/get_r_fun.R
+++ R/get_r_fun.R
@@ -1,14 +1,13 @@
 #' Function that calculates the r_(i,j) probability in the acceptance ratio
 #'
-#' @param k_int : integer in [0,kmax], current k value
-#' @param k_prop : integer in [0,kmax], either k_int - 1 or k_int + 1, proposed k value
+#' @param k_int : integer in 0 to kmax, current k value
+#' @param k_prop : integer in 0 to kmax, either k_int - 1 or k_int + 1, proposed k value
 #' @param kmax : integer greater than 0, max ar(k) process that can be checked
 #'
 #' @return probability of either birth or death
-#' @export
 #'
 #' @examples
-#' will add soon
+#' ## will add soon
 get_r_fun = function(k_int,k_prop,kmax){
   # returns the probability birth or death
   can_birth = as.numeric(k_int < kmax)
