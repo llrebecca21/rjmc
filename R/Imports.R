f2aa7c15bfd9a941a38246ecadff9a4b3b02f463
llrebecca21 <llrebecca21@gmail.com>
2024-04-17 17:32:41 -0500

    Update for documentation and package building purposes.

--- R/Imports.R
+++ R/Imports.R
@@ -1,5 +1,9 @@
-#' @importFrom stats rnorm dnorm runif
+#' @importFrom stats rnorm dnorm runif rgamma
 #' @importFrom mvtnorm rmvnorm
 #' @importFrom bayesplot mcmc_trace
 #' @importFrom Rdpack reprompt
+#' @import ggplot2
+#' @importFrom latex2exp TeX
+#' @importFrom tip ggplot_number_of_clusters_hist
+#' @importFrom tidyr pivot_longer
 NULL
